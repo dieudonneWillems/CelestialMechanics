85cd78b8fe1ef72a0d83ec704e5950753021cdf7
Don <don.willems@kolonial.no>
2020-10-26 10:26:41 +0100

    Initial Commit

---
+++ CelestialMechanics/CelestialMechanics.h
@@ -0,0 +1,18 @@
+//
+//  CelestialMechanics.h
+//  CelestialMechanics
+//
+//  Created by Don Willems on 26/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CelestialMechanics.
+FOUNDATION_EXPORT double CelestialMechanicsVersionNumber;
+
+//! Project version string for CelestialMechanics.
+FOUNDATION_EXPORT const unsigned char CelestialMechanicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CelestialMechanics/PublicHeader.h>
+
+
