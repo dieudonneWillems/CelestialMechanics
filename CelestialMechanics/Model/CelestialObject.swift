e0e42252d4009cd3ce806e20f33776b26d2d2b37
Don <don.willems@kolonial.no>
2020-10-29 23:14:59 +0100

    Added model for celestial objects

--- CelestialMechanics/Model/CelestialObject.swift
+++ CelestialMechanics/Model/CelestialObject.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+public protocol CelestialObject {
+    
+    func sphericalCoordinates(at epoch: Date, inCoordinateFrame frame: CoordinateFrame)
+    func rectangularCoordinates(at epoch: Date, inCoordinateFrame frame: CoordinateFrame)
+    func visualMagnitude(at epoch: Date)
+    
+}
+
+public protocol DeepSkyObject: CelestialObject {
+    
+}
+
+public protocol PointSource: CelestialObject {
+    
+}
+
+public protocol Star: DeepSkyObject, PointSource {
+    
+}
+
+public protocol VariableStar: Star {
+    
+}
+
+public protocol MultipleStar: Star {
+    
+}
+
+public protocol ExtendedDeepSkyObject: DeepSkyObject {
+    
+}
+
+public protocol SolarSystemObject: CelestialObject {
+    
+}
+
+public protocol Planet: SolarSystemObject {
+    
+}
+
+public protocol DwarfPlanet: Planet {
+    
+}
+
+public protocol MinorPlants: SolarSystemObject {
+    
+}
+
+public protocol Comet: SolarSystemObject {
+    
+}
+
+public protocol Satellite: SolarSystemObject {
+    
+}
