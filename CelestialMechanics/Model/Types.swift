1ab028f735a2c0079c2236e134f290bdc6cfc028
Don <don.willems@kolonial.no>
2020-10-29 23:29:14 +0100

    Added Magnitude simple type

---
+++ CelestialMechanics/Model/Types.swift
@@ -0,0 +1,16 @@
+//
+//  Types.swift
+//  CelestialMechanics
+//
+//  Created by Don Willems on 29/10/2020.
+//
+
+import Foundation
+
+public struct Magnitude {
+    
+    let value: Double
+}
+
+
+// TODO: Operator overloading.
